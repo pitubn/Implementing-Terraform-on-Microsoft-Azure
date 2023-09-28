081591202130cbee5cbe81862f5efc4258bf8402
pitubn <113511861+pitubn@users.noreply.github.com>
2023-09-28 12:06:17 +0200

    second

--- 1-main-vnet/main.tf
+++ 1-main-vnet/main.tf
@@ -21,18 +21,18 @@ variable "resource_group_name" {
 
 variable "location" {
   type    = string
-  default = "eastus"
+  default = "ukwest"
 }
 
 
 variable "vnet_cidr_range" {
   type    = string
-  default = "10.0.0.0/16"
+  default = "10.5.0.0/16"
 }
 
 variable "subnet_prefixes" {
   type    = list(string)
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
+  default = ["10.5.0.0/24", "10.5.1.0/24"]
 }
 
 variable "subnet_names" {
